6b2346719038568d57afcee742c65a88515d3833
ObaHebun <imthedamnedone@gmail.com>
2024-07-01 01:03:11 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+dz_made = 12
+hours_spent = 1.5
+course_name = 'Python'
+one_task = (hours_spent / dz_made)
+print('Курс:',course_name,', всего задач:', dz_made, ', затрачено часов:', hours_spent, ', среднее время выполнения:', one_task)
